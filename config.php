c3c4609e5508304a86b5c1bf31e144e89c137b5f
Manyonne <ptitesailes@gmail.com>
2024-04-10 12:52:30 +0200

    update

---
+++ config.php
@@ -0,0 +1,10 @@
+/* config.php */
+
+<?php
+
+class config {
+    const SERVEUR = 'localhost';
+    const BASEDEDONNEES = 'sncf_connect_db';
+    const UTILISATEUR = 'root';
+    const MOTDEPASSE = '';
+}
